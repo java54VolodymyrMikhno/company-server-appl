6657616efde21912f49e35feef1313995b2fd790
VolodymyrMikhno <volodymyr3583@gmail.com>
2024-07-31 09:23:09 +0300

    added shutdown companyServer application

--- src/main/java/telran/employees/net/CompanyServerAppl.java
+++ src/main/java/telran/employees/net/CompanyServerAppl.java
@@ -5,23 +5,54 @@ import telran.io.Persistable;
 import telran.net.Protocol;
 import telran.net.TcpServer;
 
-public class CompanyServerAppl {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class CompanyServerAppl {
 	private static final String FILE_NAME = "employeesTest.data";
 	private static final int PORT = 5000;
 
 	public static void main(String[] args) {
-		Company company =  new CompanyMapsImpl();
+		Company company = new CompanyMapsImpl();
 		try {
-			 company = new CompanyMapsImpl();
-			((Persistable)company).restore(FILE_NAME);
+			((Persistable) company).restore(FILE_NAME);
 		} catch (Exception e) {
-			
+			e.printStackTrace();
 		}
+
 		Protocol protocol = new CompanyProtocol(company);
-		TcpServer tspServer = new TcpServer(protocol, PORT);
-		tspServer.run();
+		TcpServer tcpServer = new TcpServer(protocol, PORT);
 
+		Thread serverThread = new Thread(tcpServer);
+		serverThread.start();
+		boolean shutdownFlag = false;
+
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			String command;
+			System.out.println("Enter 'shutdown' to stop the server:");
+			while (!shutdownFlag && (command = reader.readLine()) != null) {
+
+				if ("shutdown".equalsIgnoreCase(command)) {
+					tcpServer.shutdown();
+					shutdownFlag = true;
+					while (serverThread.isAlive()) {
+						try {
+							serverThread.join(100);
+						} catch (InterruptedException e) {
+							throw new RuntimeException(e.getMessage());
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e.getMessage());
+		}
+
+		try {
+			((Persistable) company).save(FILE_NAME);
+			System.out.println("Data saved, Server stopped");
+		} catch (Exception e) {
+			System.out.println("Failed to save data");
+		}
 	}
-
 }
